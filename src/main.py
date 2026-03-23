a6c93913273d38663a73ef28461955d487471616
Bochra <a.bochra@live.ca>
2026-03-23 23:02:28 +0100

    Modification du main.py : ajout de plusieurs dictionnaires ou listes nécessaires pour le stockage de valeurs importantes aux stratégies comme les regrets cumulés pour chaque fiole pour chaque joueur, les scores pour chaque fiole à la fin de chaque épisode et encore

--- src/main.py
+++ src/main.py
@@ -21,17 +21,16 @@ import pySpriteWorld.glo
 from search.grid2D import ProblemeGrid2D
 from search import probleme
 
-
-#Strat 
-
+#-------------------------------
+# Stratégies
 from strategies import strategie_aleatoire_uniforme, strategie_tetu, strategie_aleatoire_coordination, strategie_fictitious_play, strategie_regret_matching
 
 # Choisir une stratégie pour chaque équipe
 strategie_eq = [strategie_tetu, strategie_aleatoire_uniforme]
-#l’équipe 0 utilisera strategie_tetu
-#l’équipe 1 utilisera strategie_aleatoire_uniforme
+# strategie_eq[0] correspond à la stratégie utilisée par l'équipe 0
+# strategie_eq[1] correspond à la stratégie utilisée par l'équipe 1
 
-
+#-------------------------------
 
 # ---- ---- ---- ---- ---- ----
 # ---- Main                ----
@@ -179,77 +178,105 @@ def main():
         return are_here
 
     # -------------------------------
-    # STRATÉGIES
+    # ETATS DES STRATEGIES
+    # -------------------------------
+    # Pour la stratégie têtu, on aura besoin d'une liste de deux dictionnaires qui contiennent les positions pour chaque joueur de l'équipe {player : (fiole,pos)} 
+    tetu_etat = [{},{}]
+
+    # Pour la stratégie fictitious play, on aura besoin d'une liste de deux dictionnaires qui contiennent le nombre de visites par l'adversaire pour chaque fiole {fiole : nb_visites_adversaire}
+    # Donc fp_etat[0] contient le nombre de visites par fiole de l'équipe 1
+    fp_etat = [{},{}]
+
+    # Pour la stratégie regret matching, on aura besoin d'une liste de deux listes de nb_players_team dictionnaires (un pour chaque joueur) qui contiennent le regret cumulé pour chaque fiole {fiole : regret_cumule}
+    rm_etat = [[{} for _ in range(nb_players_team)] for _ in range(2)]
+
+    # Pour mettre à jour les regrets et le nombre de visites de chaque fiole par chaque équipe, il faut une liste de deux listes (choix) et un dictionnaire (pour les scores à chaque fiole)
+    choix_etats = [[],[]]
+    scores_etats = {}  # On aura {fiole : (pts_eq0, pts_eq1)}
+
+    # -------------------------------
+    # EPISODES
     # -------------------------------
 
-    # Stratégies stationnaires simples
-
-
-
-
-# Stratégies stationnaires simples ######NOUVEAU DANS LE CODE !!!!! ici : 
-    historique_fioles = {}  # clé = fiole, valeur = nombre de fois choisie
-
     for e in range(nb_episodes):
-        priority=[0,1] if e % 2 == 0 else [1,0] # ordre de priorité inversé
-        for t in priority:
-            print(f"Team {t} utilise la stratégie : {strategie_eq[t].__name__}")
-            path = []
-            choix_fiole = []
-            choix_pos = []
-            # Si stratégie coordonnée
-            if strategie_eq[t].__name__ == "strategie_aleatoire_coordination":
+        priority=[0,1] if e % 2 == 0 else [1,0] # ordre de priorité inversé à chaque épisode
+
+        # Si ce n'est pas le premier épisode, il faut mettre à jour les états pour les stratégies fictitious play et regret matching
+        if e > 0:
+            for t in [0,1]: # Pour chaque équipe
+                adversaire = 1 - t
+                # Fictitious play -> on doit mettre à jour le nombre de visites pour les fioles de l'équipe adverse
+                if strategie_eq[t].__name__ == "strategie_fictitious_play":
+                    for fiole_advers in choix_fiole[adversaire]:  # Pour chaque fiole visitée par l'équipe adversaire, on incrémente le compteur de visites
+                        fp_etat[t][fiole_advers] = fp_etat[t].get(fiole_advers, 0) + 1
+
+                # Regret matching : calculer et cumuler les regrets
+                if strategie_eq[t].__name__ == "strategie_regret_matching":
+                    for p in range(nb_players_team):  # Pour chaque joueur de l'équipe t
+                        fiole_p = choix_fiole[t][p]   # On récupère la fiole jouée par le joueur p dans l'équipe t à l'épisode précédent
+                        score_obtenu = scores_etats.get(fiole_p, (0,0))[t]  # On récupère le score obtenu du joueur p dans l'équipe t à l'épisode précédent
+                        for f in items:
+                            score_hypothese = scores_etats.get(f, (0,0))[t]  # Calcul du score qu'on aurait pu avoir en allant sur la fiole f
+                            regret = max(0.0, score_hypothese - score_obtenu)   # Calcul du regret de n'avoir pas joué la fiole f
+                            rm_etat[t][p][f] = rm_etat[t][p].get(f, 0.0) + regret   # Stockage du regret cumulé du joueur p de l'équipe t de n'avoir pas joué la fiole f 
+
+        # Chaque équipe va choisir sa position et sa fiole cible simultanément
+        choix_fiole = [[],[]]
+        choix_pos = [[],[]]
+
+        for t in [0,1]:
+            nom = strategie_eq[t].__name__
+            print(f"Team {t} utilise la stratégie : {nom}")
+            
+            # Selon la stratégie, on va faire appel à la fonction avec les bons arguments
+            if nom == "strategie_tetu":
+                for p in range(nb_players_team):
+                    f, pos = strategie_eq[t](team[t][p], items, around_pos_free, prev_choices=tetu_etat[t])
+                    choix_fiole[t].append(f)
+                    choix_pos[t].append(pos)
+
+            elif nom == "strategie_aleatoire_uniforme":
+                for p in range(nb_players_team):
+                    f, pos = strategie_eq[t](team[t][p], items, around_pos_free)
+                    choix_fiole[t].append(f)
+                    choix_pos[t].append(pos)
+
+            elif nom == "strategie_aleatoire_coordination":
                 choix = strategie_eq[t](team[t], items, around_pos_free)
+                for f, pos in choix:
+                    choix_fiole[t].append(f)
+                    choix_pos[t].append(pos)
+            
+            elif nom == "strategie_fictitious_play":
                 for p in range(nb_players_team):
-                    f, pos = choix[p]
-                    choix_fiole.append(f)
-                    choix_pos.append(pos)
-            else:
+                    f, pos = strategie_eq[t](team[t][p], items, around_pos_free, prev_choices_adv=fp_etat[t])
+                    choix_fiole[t].append(f)
+                    choix_pos[t].append(pos)
+            
+            elif nom == "strategie_regret_matching":
                 for p in range(nb_players_team):
-                    # on passe l'historique si la stratégie a un param prev_choices
-                    f, pos = strategie_eq[t](team[t][p], items, around_pos_free, prev_choices=historique_fioles)
-                    choix_fiole.append(f)
-                    choix_pos.append(pos)
+                    f, pos = strategie_eq[t](team[t][p], items, around_pos_free, regrets=rm_etat[t][p])
+                    choix_fiole[t].append(f)
+                    choix_pos[t].append(pos)
 
-            # ---- Après avoir choisi les fioles et positions, mettre à jour l'historique ----
-            for f in choix_fiole:
-                historique_fioles[f] = historique_fioles.get(f, 0) + 1
+        # -------------------------------
+        # DEPLACEMENTS VERS LES POSITIONS ET FIOLES CIBLES
+        # -------------------------------
 
-
-
-
-       
-
-    # -------------------------------
-    # Strategie aleatoire
-    # -------------------------------
-
-
-    for e in range(nb_episodes):
-        priority=[0,1] if e % 2 == 0 else [1,0] # ordre de priorité inversé pour chaque épisode
         for t in priority:
             print("Team ",t)
             path = []
-            choix_fiole = []
-            choix_pos = []
-            for p in range(0,nb_players_team):
-                f = random.choice(items)
-                while busy(f.get_rowcol()): # si plus de place on choisit une autre fiole
-                    f = random.choice(items)
-                choix_fiole.append(f)
-                # choisir une position libre autour de la fiole choisie
-                chosen_pos = random.choice(around_pos_free(f.get_rowcol()))
-                choix_pos.append(chosen_pos)
-
+            for p in range(nb_players_team):
                 pos_player = team[t][p].get_rowcol()
-                print("Player ", p, " starting from ", pos_player, " going to potion ", choix_fiole[p].get_rowcol(), " at ", choix_pos[p])
+                pos_cible = choix_pos[t][p]
+                fiole_cible = choix_fiole[t][p]
+                print("Player ", p, " starting from ", pos_player, " going to potion ", fiole_cible.get_rowcol(), " at ", pos_cible)
 
                 # -------------------------------
                 # calcul A* pour le joueur
                 # -------------------------------
 
                 g = np.ones((nb_lignes, nb_cols), dtype=bool)  # une matrice remplie par defaut a True
-
                 for i in range(nb_lignes):  # on exclut aussi les bordures du plateau
                     g[0][i] = False
                     g[1][i] = False
@@ -263,47 +290,36 @@ def main():
                 path.append(probleme.astar(prob, verbose=False))
                 print("Chemin trouvé:", path[p])
 
-
                 #-------------------------------
                 # Boucle principale de déplacements
                 #-------------------------------
-
                 # on fait bouger le joueur jusqu'à son but
                 # en suivant le chemin trouve avec A*
 
                 for i in range(len(path[p])):  # si le joueur n'est pas deja arrive
                     (row, col) = path[p][i]
                     team[t][p].set_rowcol(row, col)
-                    print("pos joueur:",  row, col)
+                    print("pos joueur:",row, col)
 
-                    # mise à jour du pleateau de jeu
+                    # mise à jour du plateau de jeu
                     game.mainiteration()
 
 
-
-
-
-
-
-
         # -------------------------------
-        # Calcul des scores
+        # CALCUL DES SCORES
         # -------------------------------
 
-
-        # calcul du nombre de joueurs autour de chaque fiole
+        # Calcul du nombre de joueurs autour de chaque fiole
         for o in items:
             print(players_around_item(o))
 
-
-
-        # calcul des points
-        #TODO
-        liste_fioles = list()
+        # Calcul des points
+        liste_fioles = list() # Liste des tuples (nb_joueurs_eq0, nb_joueurs_eq1) pour chaque fiole
         score_eq0 = 0
         score_eq1 = 0
         for o in items:
             liste_fioles.append(players_around_item(o))
+
         # S'il y a égalité après vérification des conditions, aucune équipe ne prend le point
         if name == "yellow-map":    # Map avec seulement des fioles jaunes
             for nb_eq0, nb_eq1 in liste_fioles:
@@ -408,8 +424,74 @@ def main():
         score_total_eq0 += score_eq0
         score_total_eq1 += score_eq1
 
-        # remettre les joueurs à leur pos initiale a la fin de l'episode
+        # -------------------------------
+        # SAUVEGARDE DES CHOIX ET DES SCORES DE CHAQUE EQUIPE
+        # -------------------------------
+        # Après chaque épisode, il faut sauvegarder les choix et les scores pour les utiliser dans les stratégies de regret_matching et fictitious play au prochain épisode
+        choix_etats = [list(choix_fiole[0]), list(choix_fiole[1])]
+        scores_etats = {}
 
+        for i, o in enumerate(items):
+            nb_eq0, nb_eq1 = liste_fioles[i]
+            # On va recalculer les points pour chaque fiole individuellement pour le regret-matching
+            # On va donc reprendre la même logique que pour le calcul de scores
+            pts0, pts1 = 0, 0
+            couleur = "yellow"
+            if name == "yellow-map": couleur = "yellow"
+            elif name == "green-map": couleur = "green"
+            elif name == "blue-map": couleur = "blue"
+            elif name == "red-map": couleur = "red"
+            elif name == "mixed-map":
+                couleurs_mixed = ["yellow", "green", "yellow", "red", "blue", "red", "yellow", "green", "yellow"]
+                couleur = couleurs_mixed[i]
+            
+            if couleur == "yellow":
+                if nb_eq0 < 1 :
+                    if nb_eq1 >= 1:
+                        pts1 = 1
+                elif nb_eq1 < 1:
+                    pts0 = 1
+                elif nb_eq0 > nb_eq1:
+                    pts0 = 1
+                elif nb_eq1 > nb_eq0:
+                    pts1 = 1
+            elif couleur == "red":
+                cond0, cond1 = nb_eq0 >= 2, nb_eq1 >= 2
+                if cond0 and cond1:
+                    if nb_eq0 > nb_eq1:
+                        pts0 = 1
+                    elif nb_eq1 > nb_eq0:
+                        pts1 = 1
+                elif cond0:
+                    pts0 = 1
+                elif cond1:
+                    pts1 = 1
+            elif couleur == "green":
+                total = nb_eq0 + nb_eq1
+                if total >= 3:
+                    if nb_eq0 > nb_eq1:
+                        pts0 = 1
+                    elif nb_eq1 > nb_eq0:
+                        pts1 = 1
+            elif couleur == "blue":
+                cond0, cond1 = nb_eq0 >= 2, nb_eq1 >= 2
+                if nb_eq0 == 1 and cond1:
+                    pts0 = 1
+                elif nb_eq1 == 1 and cond0:
+                    pts1 = 1
+                elif cond0 and cond1:
+                    if nb_eq0 > nb_eq1:
+                        pts0 = 1
+                    elif nb_eq1 > nb_eq0:
+                        pts1 = 1
+                elif cond0:
+                    pts0 = 1
+                elif cond1:
+                    pts1 = 1
+            # On le stocke donc comme valeur de la fiole correspondante
+            scores_etats[o] = (pts0, pts1)
+
+        # Remettre les joueurs à leur position initiale a la fin de l'episode
         for i in [0,1]:
             j=0
             for p in team[i]:
